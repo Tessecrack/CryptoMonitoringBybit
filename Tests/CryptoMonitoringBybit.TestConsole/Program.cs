5aea96613dac37ee85635fd60fb039073700b6a0
sirozu <rychen123@yandex.ru>
2024-03-16 13:06:03 +0300

    configuration test console project

--- Tests/CryptoMonitoringBybit.TestConsole/Program.cs
+++ Tests/CryptoMonitoringBybit.TestConsole/Program.cs
@@ -1,11 +1,32 @@
-﻿class Program
-{
-	public static int Main(string[] args)
-	{
-		Console.WriteLine("Hello World");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
+class Program
+{
+	private static IHost __Hosting;
+
+	public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+		Host.CreateDefaultBuilder(args)
+		.ConfigureServices(ConfigureService);
+
+	public static IServiceProvider Services => Hosting.Services;
+
+	private static void ConfigureService(HostBuilderContext host, IServiceCollection services)
+	{
+		
+	}
+
+	 static async Task Main(string[] args)
+	{
+		using var host = Hosting;
+
+		await host.StartAsync();
+
+		Console.WriteLine("Done!");
 		Console.ReadKey();
 
-		return 0;
+		await host.StopAsync();
 	}
 }
